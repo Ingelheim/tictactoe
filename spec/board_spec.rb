8958009d28f02593a8baa9b4859b110295f0741e
Lukas von Ingelheim <lukas.ingelheim@gmail.com>
2013-08-09 16:12:04 +0200

    cleaned up the general structure of the game

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,54 +1,48 @@
 require 'spec_helper'
 
 describe Board do
+	let(:board) { Board.new }
+	let(:board_big) { Board.new(5) }
 
 	describe "initializes" do
 		it "should be an instance of a Shirt object" do
-			board = Board.new
 			board.should be_an_instance_of(Board)
 		end
 
 		it "initialize a board of 3x3 when not further defined" do
-			board = Board.new
 			board.size.should eq(3)
 		end
 
 		it "initialize a board of custom size when defined by user" do
-			board = Board.new(9)
-			board.size.should eq(9)
+			board_big.size.should eq(5)
 		end
 	end
 
 	describe "create board" do
 		it "creates an empty board 3x3 when no further defined" do
-			board = Board.new
 			board.create
 			board.show_board.should eq ([["E","E","E"],["E","E","E"],["E","E","E"]])
 		end
 
 		it "creates an empty board 5x5 when pre-defined" do
-			board = Board.new(5)
-			board.create
-			board.show_board.should eq ([["E","E","E","E","E"],["E","E","E","E","E"],["E","E","E","E","E"],["E","E","E","E","E"],["E","E","E","E","E"]])
+			board_big.create
+			board_big.show_board.should eq ([["E","E","E","E","E"],["E","E","E","E","E"],["E","E","E","E","E"],["E","E","E","E","E"],["E","E","E","E","E"]])
 		end
 	end
 
 	describe "update marker" do
 		it "updates the marker when field is empty and within range" do
-			board = Board.new
 			board.create
 			board.update_marker("X", [0,2])
 			board.show([0,2]).should eq("X")
 		end
 
 		it "does not update the marker when field is already occupied" do
-			board = Board.new
 			board.instance_variable_set("@board", [["E","E","E"],["E","E","X"],["E","E","E"]])
 			board.update_marker("X", [1,2]).should eq("already taken")
 		end
 
 		it "raises an error when field which should be updated is not in range" do
-			board = Board.new
 			board.create
 			board.update_marker("X", [3,2]).should eq("not on board")
 		end
